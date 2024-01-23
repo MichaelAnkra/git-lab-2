0c54590517215f513cd77db937d1aac477818a99
MichaelAnkra <ma970121@ohio.edu>
2024-01-23 13:01:44 -0500

    done

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+*       File: git-lab-program.cc
+*       Author: Michael Ankrah
+*       Date:01/23/2024
+*       DEscription: lab 2 was to help us get familiar with git and github
+*
+*
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main (int argc, char const *argv[] ) {
+
+    /*add codes*/
+    cout<<"Hello Git!!"<< endl;
+    return 0;
+}// main
